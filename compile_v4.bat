85d3d9fbb5771d94a4906493d3053553bf301020
Gray Programmerz <hackrefisher@gmail.com>
2021-05-20 12:15:06 +0500

    Create compile_v4.bat

---
+++ compile_v4.bat
@@ -0,0 +1,15 @@
+@echo off
+
+set "v4=C:\Windows\Microsoft.NET\Framework\v4.0.30319"
+
+set "vb=%v4%\vbc.exe"
+set "cs=%v4%\csc.exe"
+
+
+For /R .\tests\ %%G IN (*.cs) do (
+  %cs% /nologo /out:Bin\%%~nG_cs.exe %%G
+)
+
+For /R .\tests\ %%G IN (*.vb) do (
+  %vb% /nologo /out:Bin\%%~nG_vb.exe %%G
+)
