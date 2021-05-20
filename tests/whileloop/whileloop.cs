4d56992daf8960b9c2f24f3101ebd0fd1644c518
Gray Programmerz <hackrefisher@gmail.com>
2021-05-20 19:29:28 +0500

    Update whileloop.cs

--- tests/whileloop/whileloop.cs
+++ tests/whileloop/whileloop.cs
@@ -1,18 +1,18 @@
 using System;
 using System.Diagnostics;
 
-public class forloop
+public class whileloop
 {
 	public static void Main()
 	{
 	    Stopwatch timer = new Stopwatch();
-        timer.Start();
-        forlooptest();
-		timer.Stop();
-		Console.Write(timer.ElapsedMilliseconds.ToString());
+            timer.Start();
+            whilelooptest();
+	    timer.Stop();
+            Console.Write(timer.ElapsedMilliseconds.ToString());
 	}
 	
-	static void forlooptest()
+	static void whilelooptest()
 	{
 		long heavyNo = 1000000000;
 		long i = 0;
