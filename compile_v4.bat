739f89d3d4068db882e1c447d1c5024bd0a9376b
Gray Programmerz <hackrefisher@gmail.com>
2021-05-20 12:26:32 +0500

    Update compile_v4.bat

--- compile_v4.bat
+++ compile_v4.bat
@@ -7,9 +7,9 @@ set "cs=%v4%\csc.exe"
 
 
 For /R .\tests\ %%G IN (*.cs) do (
-  %cs% /nologo /out:Bin\%%~nG_cs.exe %%G
+  %cs% -w:0 /nologo /out:Bin\%%~nG_cs.exe %%G
 )
 
 For /R .\tests\ %%G IN (*.vb) do (
-  %vb% /nologo /out:Bin\%%~nG_vb.exe %%G
+  %vb% -w:0 /nologo /out:Bin\%%~nG_vb.exe %%G
 )
